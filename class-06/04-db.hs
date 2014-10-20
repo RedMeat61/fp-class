9b4d5105812f2dbf4feb5aaa0f74e449a63aa10c
Red_Meat <student.belousov@gmail.com>
2014-10-20 10:10:48 +0400

    4.db

--- class-06/04-db.hs
+++ class-06/04-db.hs
@@ -12,5 +12,24 @@
         ФАМИЛИЯ И.О.
 
 -}
+import Data.List.Split
+import System.Environment
+import Data.List
+import System.IO
 
-main = undefined
+import System.Directory
+import Data.Char
+import Data.String
+
+
+c1 fn (k:g:[]) = do
+	contents <- readFile fn
+	putStr $ show $ map (endBy ";" ) $ lines $ contents
+
+main = do
+	fn:args <- getArgs
+	case (head args) of
+		"1" -> c1 fn (tail args)
+		--"2" -> c2 (tail args)
+		--"3" -> c3 (tail args)
+
