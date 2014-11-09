c54c11ae19cf18a2f08e029afc9aab83cd1cd032
Red_Meat <student.belousov@gmail.com>
2014-11-09 13:58:44 +0400

    8.1

--- class-08/02-listable.hs
+++ class-08/02-listable.hs
@@ -4,10 +4,24 @@
    toList :: a -> [a]
    fromList :: [a] -> a
 -}
-
+class Listable a where
+   toList :: a -> [a]
+   fromList :: [a] -> a
 
 {-
   Объявите экземпляры класса типов Listable для следующих типов:
   1) String - строка разбивается по пробелам на список слов.
   2) Integer - любое целое число разбивается на список цифр.
 -}
+instance Listable String where
+	toList x = words x
+	fromList x = unwords x 
+	
+instance Listable Integer where
+	toList x = []++(reverse(pise x))
+		where 
+			pise x = if (x >= 10) then ([(x `mod` 10)] ++ pise (x `div` 10))
+				else x
+	fromList x = p x
+		where 
+			p x = head x + 10*(p x)
